24d350c87266cf1862a3099e4f8177accd33b296
mbsomeday <137542994@qq.com>
2024-10-18 10:15:30 +0100

    update

--- ddpm.py
+++ ddpm.py
@@ -109,7 +109,7 @@ def launch():
 
     args.run_name = "DDPM_Uncondtional"
     args.epochs = 100
-    args.batch_size = 4
+    args.batch_size = 2
 
     # args.image_size = 64
     args.image_size = 112
